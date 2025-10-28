58d916a2a4c383d50afa5deaca2c801ed087239f
p4r7h <parthos250@gmail.com>
2025-10-28 22:19:48 +0530

    added feature component and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import About from "./components/About";
 import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
-import Features from "./components/Features";
-import Story from "./components/Story";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import About from "./components/About.jsx";
+import NavBar from "./components/NavBar.jsx";
+import Features from "./components/Features.jsx";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar />
-      <Hero />
-      <About />
-      <Features />
-      <Story />
-      <Contact />
-      <Footer />
+        <NavBar />
+        <Hero/>
+        <About/>
+        <Features/>
     </main>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({title,containerClass,id,rightIcon, leftIcon}) => {
+    return (
+        <div
+        id={id}
+        className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3
+        ${containerClass}`}
+        >
+            {leftIcon}
+            <span className="relative inline-flex overflow-hidden font-general text-xl uppercase">
+                {title}
+            </span>
+        </div>
+    )
+}
+export default Button

7b460d3b18460be31d6fe837acb70624cb604e73
p4r7h <parthos250@gmail.com>
2025-11-06 22:00:24 +0530

    completed

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -76,7 +76,7 @@ const Features = () => {
                 className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
                 <BentoCard
                     src="videos/feature-1.mp4"
-                    title={<>radi<b>n</b>t</>}
+                    title={<>radia<b>n</b>t</>}
                     description = "A cross-platform metagame app, turning your activities
                     across Web2 and Web3 games into a rewarding adventure"
                 />
